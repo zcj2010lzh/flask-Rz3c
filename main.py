821d1e9f1cc98939ac333de37eeea7b2f2a0253d
zcj2010lzh <1558475353@qq.com>
2023-03-26 13:58:21 +0800

    change main.py and add template

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template, request
 import os
+import youtube_dl
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
+    return render_template('index.html')
+@app.after_request
+def cors(environ):
+    environ.headers['Access-Control-Allow-Origin']='*'
+    environ.headers['Access-Control-Allow-Method']='*'
+    environ.headers['Access-Control-Allow-Headers']='x-requested-with,content-type'
+    return environ
+@app.route('/getVideoInfo',methods=['GET'])
+def get_video_info():
+    message = request.args.get('videoUrl')
+    # print(message)
+        # 创建 youtube-dl 对象
+    ydl = youtube_dl.YoutubeDL()
+    
+    # 获取视频信息
+    info = ydl.extract_info(message, download=False)
+    
+    # 获取视频的下载链接
+    video_url = info['url']
+    
+    print('视频下载链接：', video_url)
+    # 在这里对数据进行处理
+    return  video_url
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
